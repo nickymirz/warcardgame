4c8dd9253146f7fddbb2c19bb497ac4fa414c885
Nicky Mirz <nickymirz@gmail.com>
2020-08-13 19:20:46 +0300

    Updates

---
+++ precache-manifest.f42ba06afe2f46cb0815704a66ba60b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e871a7243ce488bb57a3f3f26b1b21",
+    "url": "/warcardgame/index.html"
+  },
+  {
+    "revision": "6a93cb29787af48162a1",
+    "url": "/warcardgame/static/css/main.62b69b9c.chunk.css"
+  },
+  {
+    "revision": "eb464e07028dba0fbc06",
+    "url": "/warcardgame/static/js/2.210ff24e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/warcardgame/static/js/2.210ff24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a93cb29787af48162a1",
+    "url": "/warcardgame/static/js/main.f482ca1d.chunk.js"
+  },
+  {
+    "revision": "1969767e9c9c3ad18631",
+    "url": "/warcardgame/static/js/runtime-main.6aa3d23f.js"
+  }
+]);
